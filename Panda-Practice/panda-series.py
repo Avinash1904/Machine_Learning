2307136073c7dae553b0fc9cf5fb011b45fd638f
Avinash <avinashmishra1904@gmail.com>
2018-07-22 12:18:28 +0530

    commited folder

---
+++ Panda-Practice/panda-series.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import pandas as pd
+#s = pd.Series()
+s = pd.Series([1,2,3,4,5],index = ['a','b','c','d','e'])
+
+print(s)
+
+
